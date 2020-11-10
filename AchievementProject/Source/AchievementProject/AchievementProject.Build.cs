f7f4cef9ce474e3599364ae16829ced44f66effb
ironm102 <yunis.albreihi@gmail.com>
2020-11-10 13:36:21 +0100

    Added values to the build so Euditor Utlity widgets work.

--- AchievementProject/Source/AchievementProject/AchievementProject.Build.cs
+++ AchievementProject/Source/AchievementProject/AchievementProject.Build.cs
@@ -4,10 +4,14 @@ using UnrealBuildTool;
 
 public class AchievementProject : ModuleRules
 {
-	public AchievementProject(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public AchievementProject(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
-	}
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "Blutility", "UMG", "UnrealEd", "Slate", "SlateCore" });
+
+        PrivateDependencyModuleNames.AddRange(new string[] { });
+
+        PublicIncludePaths.AddRange(new string[] { System.IO.Path.GetFullPath(Target.RelativeEnginePath) + "Source/Editor/Blutility/Private" });
+    }
 }
